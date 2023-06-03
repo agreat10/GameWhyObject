0762a834d4f4e2d2406cd1e87d4136c7026f64aa
agreat10 <komolov10@gmail.com>
2023-06-03 17:19:30 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmogi = new List<string>()
+            {
+                "❤","❤",
+                "🎃","🎃",
+                "👓","👓",
+                "🤑","🤑",
+                "👩‍🚀","👩‍🚀",
+                "💎","💎",
+                "🎁","🎁",
+                "⚽","⚽"
+            };
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>() )
+            {
+                int index = random.Next(animalEmogi.Count);
+                string nextEmogi = animalEmogi[index];
+                textBlock.Text = nextEmogi;
+                animalEmogi.RemoveAt(index);
+            }
+        }
+    }
+}
